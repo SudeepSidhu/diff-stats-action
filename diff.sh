f498544db9ebe0352babed5036b738d39ad631f3
Sudeep Sidhu <sudeep.sidhu@neofinancial.com>
2021-08-20 17:53:35 -0600

    Pass in DIFF_OPTIONS

--- diff.sh
+++ diff.sh
@@ -32,7 +32,7 @@ set -o xtrace
 git checkout $BASE_BRANCH && git pull
 git checkout $HEAD_BRANCH && git pull
 
-OUTPUT=$(git diff $BASE_BRANCH...$BASE_BRANCH --stat ${})
+OUTPUT=$(git diff $BASE_BRANCH...$BASE_BRANCH --stat ${DIFF_OPTIONS})
 
 echo $OUTPUT
 
